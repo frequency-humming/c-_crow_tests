251037968245b1e268f5671042fb73640ceeaa3e
Bruno Signori <signoribruno@gmail.com>
2024-01-01 17:44:42 -0500

    added refresh of main components

--- main2.cpp
+++ main2.cpp
@@ -23,19 +23,49 @@ int main() {
         return page.render(ctx);
     });
 
-    CROW_ROUTE(app, "/test")
+    CROW_ROUTE(app, "/tools")
     ([] {
-        auto page = crow::mustache::load("test.html");
+        auto page = crow::mustache::load("trace.html");
         return page.render();
     });
 
-    CROW_ROUTE(app, "/send").methods("POST"_method)([&tracerouteFuture](const crow::request& req) {
+    CROW_ROUTE(app, "/sendendpoint").methods("POST"_method)([&tracerouteFuture](const crow::request& req) {
         std::string endpoint = req.body;
         tracerouteFuture = runTracerouteAsync(endpoint);
         return crow::response(202);
     });
+#ifdef __APPLE__
+    CROW_ROUTE(app, "/stats").methods("POST"_method)([&stats]() {
+        crow::mustache::context ctx;
+        for (const auto& stat : stats) {
+            if (stat.getName() == "cpuUsage") {
+                ctx[stat.getName()] = execCommand("top -l 1 | grep CPU", std::bitset<4>{0b0100});
+            } else if (stat.getName() == "memoryUsage") {
+                ctx[stat.getName()] = execCommand("top -l 1 | grep PhysMem", std::bitset<4>{0b0000});
+            } else if (stat.getName() == "diskUsage") {
+                ctx[stat.getName()] = execCommand("top -l 1 | grep Disk", std::bitset<4>{0b0100});
+            } else if (stat.getName() == "networkUsage") {
+                ctx[stat.getName()] = execCommand("top -l 1 | grep Network", std::bitset<4>{0b0000});
+            } else {
+                continue;
+            }
+        }
+        return ctx;
+    });
+#else
+    CROW_ROUTE(app, "/memory").methods("POST"_method)([&stats]() {
+        crow::mustache::context ctx;
+        for (const auto& stat : stats) {
+            if (stat.getName() == "cpuUsage") {
+                ctx[stat.getName()] = execCommand("mpstat -P ALL 1 | head -n 8 |  awk '/(4 CPU)/ {found=1; next} found'", std::bitset<4>{0b0010});
+                break;
+            }
+        }
+        return ctx;
+    });
+#endif
 
-    CROW_ROUTE(app, "/result")
+    CROW_ROUTE(app, "/results")
     ([&tracerouteFuture] {
         if (tracerouteFuture.valid() && tracerouteFuture.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
             return tracerouteFuture.get();
